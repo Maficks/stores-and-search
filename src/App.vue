08e2de17639593b2f20b44db3e13d4e4ea9d50d8
Maficks <berrrserk2020@mail.ru>
2024-12-21 19:24:15 +0500

    major update: restyle, grid, redo of logic, again maintained all device and sizes proper appearance with media-calls and more

--- src/App.vue
+++ src/App.vue
@@ -33,46 +33,47 @@ onMounted(() => {
         Search
       </button>
     </div>
-    <div class="recipes" v-if="recipesStore.activeTab === 1">
-      <div class="searchInput">
+    <div class="main" v-if="recipesStore.activeTab === 1">
+      <form class="searchInput">
         <input type="search" v-model="favouriteSearchText" @input="recipesStore.findRecipes(favouriteSearchText)">
-      </div>
-      <div
-          class="recipe"
-          v-for="recipe of recipesStore.recipes"
-          :key="recipe.id"
-          v-if="recipesStore.searching === false"
-      >
-        <img :src="recipe.image" alt="recipeImage" width="10%" height="auto">
-        <span>Number: {{ recipe.id }}</span>
-        <span>{{ recipe.name }}</span>
-        <button @click="recipesStore.deleteRecipe(recipe)">Remove</button>
-        <span
-            v-for="recipeIngredient of recipe.ingredients"
-            :key="recipeIngredient.id"
-            class="ingredients"
-        >
-          {{ recipeIngredient }},
-        </span>
-        <span class="gap"></span>
-        <span
-            v-for="recipeInstruction of recipe.instructions"
-            :key="recipeInstruction.id"
-        >
-          {{ recipeInstruction }}
-        </span>
-        <span class="gap"></span>
-        <span>Preparation time(minutes): {{ recipe.prepTimeMinutes }}</span>
-        <span>Cooking time(minutes): {{ recipe.cookTimeMinutes }}</span>
-      </div>
-      <div v-else>
-        <!-- TODO: сделать стилей еще-->
+      </form>
+      <div class="recipes">
         <div
+            class="recipe"
+            v-for="recipe of recipesStore.recipes"
+            :key="recipe.id"
+            v-if="recipesStore.searching === false"
+        >
+          <img :src="recipe.image" alt="recipeImage" width="30%" height="auto">
+          <span>Number: {{ recipe.id }}</span>
+          <span>{{ recipe.name }}</span>
+          <button @click="recipesStore.deleteRecipe(recipe)">Remove</button>
+          <span
+              v-for="recipeIngredient of recipe.ingredients"
+              :key="recipeIngredient.id"
+              class="ingredients"
+          >
+            {{ recipeIngredient }},
+          </span>
+          <span class="gap"></span>
+          <span
+              v-for="recipeInstruction of recipe.instructions"
+              :key="recipeInstruction.id"
+          >
+            {{ recipeInstruction }}
+          </span>
+          <span class="gap"></span>
+          <span>Preparation time(minutes): {{ recipe.prepTimeMinutes }}</span>
+          <span>Cooking time(minutes): {{ recipe.cookTimeMinutes }}</span>
+        </div>
+        <div
+            v-else
             v-for="favouriteSearchedRecipe of recipesStore.searchingRecipes"
             :key="favouriteSearchedRecipe.id"
             class="recipe"
         >
-          <img :src="favouriteSearchedRecipe.image" alt="recipeImage" width="10%" height="auto">
+          <!-- TODO: сделать стилей еще-->
+          <img :src="favouriteSearchedRecipe.image" alt="recipeImage" width="30%" height="auto">
           <span>Number: {{ favouriteSearchedRecipe.id }}</span>
           <span>{{ favouriteSearchedRecipe.name }}</span>
           <button @click="recipesStore.deleteRecipe(favouriteSearchedRecipe)">Remove</button>
@@ -95,35 +96,37 @@ onMounted(() => {
         </div>
       </div>
     </div>
-    <div class="recipes" v-else>
-      <div class="searchInput">
+    <div class="main" v-else>
+      <form class="searchInput">
         <input type="search" v-model="searchText" @input="searchStore.getRecipes(searchText)">
-      </div>
-      <div
-          class="recipe"
-          v-for="searchedItem of searchStore.recipes"
-          :key="searchedItem.id"
-      >
-        <img :src="searchedItem.image" alt="recipeImage" width="10%" height="auto">
-        <span>Number: {{ searchedItem.id }}</span>
-        <span>{{ searchedItem.name }}</span>
-        <button @click="searchStore.addRecipe(searchedItem)">Add</button>
-        <span
-            v-for="searchedItemIngredient of searchedItem.ingredients"
-            :key="searchedItemIngredient.id"
+      </form>
+      <div class="recipes">
+        <div
+            class="recipe"
+            v-for="searchedItem of searchStore.recipes"
+            :key="searchedItem.id"
         >
-          {{ searchedItemIngredient }},
-        </span>
-        <span class="gap"></span>
-        <span
-            v-for="searchedItemInstruction of searchedItem.instructions"
-            :key="searchedItemInstruction.id"
-        >
-          {{ searchedItemInstruction }}
-        </span>
-        <span class="gap"></span>
-        <span>Preparation time(minutes): {{ searchedItem.prepTimeMinutes }}</span>
-        <span>Cooking time(minutes): {{ searchedItem.cookTimeMinutes }}</span>
+          <img :src="searchedItem.image" alt="recipeImage" width="30%" height="auto">
+          <span>Number: {{ searchedItem.id }}</span>
+          <span>{{ searchedItem.name }}</span>
+          <button @click="searchStore.addRecipe(searchedItem)">Add</button>
+          <span
+              v-for="searchedItemIngredient of searchedItem.ingredients"
+              :key="searchedItemIngredient.id"
+          >
+            {{ searchedItemIngredient }},
+          </span>
+          <span class="gap"></span>
+          <span
+              v-for="searchedItemInstruction of searchedItem.instructions"
+              :key="searchedItemInstruction.id"
+          >
+            {{ searchedItemInstruction }}
+          </span>
+          <span class="gap"></span>
+          <span>Preparation time(minutes): {{ searchedItem.prepTimeMinutes }}</span>
+          <span>Cooking time(minutes): {{ searchedItem.cookTimeMinutes }}</span>
+        </div>
       </div>
     </div>
   </body>
@@ -153,10 +156,12 @@ header {
   margin-top: 1%;
 }
 .recipes {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  padding: 20px;
+  max-width: 1920px;
+  margin: 0 auto;
 }
 .navButtons {
   display: flex;
@@ -166,17 +171,21 @@ header {
 .searchInput {
   display: flex;
   justify-content: center;
-  margin-bottom: 1%;
 }
 button {
   margin: 0 1em;
 }
 .recipe {
-  margin-bottom: 2.5%;
-  position: relative;
-  display: flex;
-  flex-direction: column;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 2%;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s;
   align-items: center;
+  flex-direction: column;
+  display: flex;
 }
 .btn-green {
   background-color: limegreen;
@@ -186,5 +195,22 @@ button {
 }
 .gap {
   margin: 1%;
+}
+.recipe:hover {
+  transform: translateY(-10px);
+}
+@media (max-width: 1100px) {
+  .recipes {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+@media (max-width: 560px) {
+  .recipes {
+    grid-template-columns: 1fr;
+  }
+}
+.main {
+  width: 100%;
+  max-width: 100vw;
 }
 </style>
